068491f02c5c4a44c35aaa74da9240121f47f536
Cadman <jcadiente505@gmail.com>
2018-08-24 22:28:04 -0400

    finished signup page

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -6,6 +6,11 @@ module.exports = function(app) {
     res.render("index")
   });
 
+  // Load signup page
+  app.get("/signup", function(req, res) {
+    res.render("signup")
+  });
+
   // Load example page and pass in an example by id
   app.get("/user/:id", function(req, res) {
     db.User.findOne({ where: { id: req.params.id } }).then(function(user) {
