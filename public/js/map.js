37929bab3a88711781848e0a044c3ae7beb2ce4c
Daniel Lopez <edwindanlopez@gmail.com>
2018-08-29 18:36:07 -0400

    Updated nav styling, links, and initial get route for sending user to user page and logging out

--- public/js/map.js
+++ public/js/map.js
@@ -258,11 +258,15 @@ function initMap() {
       var directionsDisplay = new google.maps.DirectionsRenderer;
 
       function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+
+        // var fFMapIcon = "../images/Logo-Export/Food-Finder-Map-Marker.png";
         directionsService.route(
           {
             origin: userlatlng.toString(),
             destination: restlatlng.toString(),
             travelMode: "DRIVING"
+            // icon: fFMapIcon,
+            // optimized: false
           },
           function(response, status) {
             if (status === "OK") {
@@ -277,6 +281,18 @@ function initMap() {
       calculateAndDisplayRoute(directionsService, directionsDisplay);
       directionsDisplay.setMap(map);
     });
-  }
+  };
 }
 console.log(initMap);
+
+
+$("#my-profile").on("click", function() {
+  console.log("Going to user profile");
+  $.ajax({
+    type: "GET",
+    url: "/",
+    success: function(result) {
+      $("#div1").html(result);
+    }
+  });
+});
